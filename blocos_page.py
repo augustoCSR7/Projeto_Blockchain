981a3b63829c51d200100237c24f4f0ceacb96e7
Augusto César <90866224+augustoCSR7@users.noreply.github.com>
2024-05-27 17:14:52 -0400

    v2

--- blocos_page.py
+++ blocos_page.py
@@ -1,7 +1,12 @@
 import flet as ft
-
+import time
+from datetime import datetime
 
 def format_block(block):
+    # Converte o timestamp em segundos para um objeto datetime
+    timestamp_int = int(block.timestamp)
+    dt_object = datetime.fromtimestamp(timestamp_int)
+
     return ft.Card(
         content=ft.Container(
             bgcolor=ft.colors.WHITE24,
@@ -9,7 +14,7 @@ def format_block(block):
             border_radius=ft.border_radius.all(20),
             content=ft.Text(
                 f"Index: {block.get_index()}\n"
-                f"Timestamp: {block.timestamp}\n"
+                f"Timestamp: {dt_object.strftime('%Y-%m-%d %H:%M:%S')}\n"
                 f"Previous Hash: {block.previous_hash}\n"
                 f"Data: {block.data}\n"
                 f"Nonce: {block.nonce}\n"
@@ -17,7 +22,6 @@ def format_block(block):
             ),
         )
     )
-
 
 def blocos_page(page: ft.Page, blockchain):
     formatted_blocks = [format_block(block) for block in blockchain.chain]
